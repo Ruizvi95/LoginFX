add944827dd39393e67cb905f4d24ca2146da506
ruizv <21748226@live.uem.es>
2018-10-31 19:34:01 +0100

    Primer commit

---
+++ src/application/Controlador.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Controlador {
+	@FXML
+	private Stage stage1;
+	
+	@FXML
+	public void newVentana() {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("CreacionCuenta.fxml"));
+			
+			AnchorPane ventanaDos = (AnchorPane) loader.load();
+
+			
+			stage1.close();			
+			
+			Stage stage = new Stage();
+			
+			
+			
+			Scene scene = new Scene(ventanaDos);
+			stage.setTitle("Creación de cuenta");
+			stage.setScene(scene);
+			stage.show();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
